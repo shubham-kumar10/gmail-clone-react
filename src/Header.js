d6374fe7228a08e00fd8920bb683f7396ac39b5a
shubham.snu@gmail.com <shubham.snu@gmail.com>
2020-12-30 12:45:03 +0530

    feat: App init

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h2>THis is a header</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
