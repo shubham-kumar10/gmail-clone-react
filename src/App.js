eb1d02a32febab8c669a06ba9792e7dddd2ad3be
shubham.snu@gmail.com <shubham.snu@gmail.com>
2020-12-31 15:08:47 +0530

    feat: wired compose mail

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import Mail from './Mail'
 import EmailList from './EmailList'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Composemail from './ComposeMail'
+import { selectIsComposeOpen } from './features/mailSlice'
+import { useSelector } from 'react-redux'
 
 function App() {
+    const isComposeOpen = useSelector(selectIsComposeOpen)
     return (
         <Router>
             <div className="App">
@@ -21,10 +24,10 @@ function App() {
                         </Route>
                         <Route path="/">
                             <EmailList />
-                            <Composemail />
                         </Route>
                     </Switch>
                 </div>
+                {isComposeOpen && <Composemail />}
             </div>
         </Router>
     )
